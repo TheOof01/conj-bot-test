e2aca47f381e6e914bdb014c45b3eac48eba8501
TheOof01 <59071492+TheOof01@users.noreply.github.com>
2022-02-08 15:29:28 +0000

    ok should be finished now

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ conjnumber = 0
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
 
 driver = webdriver.Chrome(options=chrome_options)
 
